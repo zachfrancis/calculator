9d500c15511f5329503f8dc3f24091a93469c5f2
Zach Francis <zacharyjamesfrancis@gmail.com>
2026-03-10 13:56:33 -0600

    Add basic math functions

---
+++ script.js
@@ -0,0 +1,31 @@
+const add = function (x, y) {
+  const sum = x + y;
+  return sum;
+};
+
+const subtract = function (x, y) {
+  const difference = x - y;
+  return difference;
+};
+
+const multiply = function (x, y) {
+  const product = x * y;
+  return product;
+};
+
+const divide = function (x, y) {
+  const quotient = x / y;
+  return quotient;
+};
+
+console.log("Add 3 + 5");
+console.log(add(3, 5));
+
+console.log("Subtract 8 - 5");
+console.log(subtract(8, 5));
+
+console.log("Multiply 4 * 5");
+console.log(multiply(4, 5));
+
+console.log("Divide 15 / 3");
+console.log(divide(15, 3));
