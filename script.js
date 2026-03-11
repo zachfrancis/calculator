1d0cb9b6cd73d2776330877c178fecbf97bb009a
Zach Francis <zacharyjamesfrancis@gmail.com>
2026-03-11 11:12:12 -0600

    Refactor and round decimal numbers

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const MAX_DECIMALS = 6;
+
 const add = function (x, y) {
   const sum = x + y;
   return sum;
@@ -18,62 +20,81 @@ const divide = function (x, y) {
   return quotient;
 };
 
+function roundNumber(n) {
+  return !Number.isInteger(n) ? n.toFixed(MAX_DECIMALS) : n;
+}
+
 function operate(operator, leftNumber, rightNumber) {
   x = Number(leftNumber);
   y = Number(rightNumber);
   switch (operator) {
     case "add":
-      return add(x, y);
+      result = add(x, y);
+      break;
     case "subtract":
-      return subtract(x, y);
+      result = subtract(x, y);
+      break;
     case "multiply":
-      return multiply(x, y);
+      result = multiply(x, y);
+      break;
     case "divide":
-      return divide(x, y);
+      result = divide(x, y);
+      break;
     default:
       return;
   }
+  return roundNumber(result);
 }
 
-function reset(display) {
+function resetDisplay() {
   display.textContent = "";
   operator = undefined;
   leftNumber = undefined;
   rightNumber = undefined;
 }
 
+function handleNumber(elem) {
+  // If last operation was equals, start with a blank slate
+  if (operator === "equals") resetDisplay(display);
+  display.textContent += elem.id;
+}
+
+function handleOperator(elem) {
+  if (leftNumber && operator !== "equals") {
+    rightNumber = display.textContent.slice(leftNumber.length + 3);
+    display.textContent = operate(operator, leftNumber, rightNumber);
+  }
+  leftNumber = display.textContent;
+  operator = elem.id;
+  if (operator !== "equals") {
+    display.textContent += ` ${elem.textContent} `;
+  }
+}
+
 function handleButtonClick(event) {
-  const display = document.querySelector(".display");
   switch (event.target.className) {
     case "number":
-      if (operator === "equals") reset(display);
-      display.textContent += event.target.id;
+      handleNumber(event.target);
       break;
 
     case "operator":
-      if (leftNumber && operator !== "equals") {
-        rightNumber = display.textContent.slice(leftNumber.length + 3);
-        display.textContent = operate(operator, leftNumber, rightNumber);
-      }
-      leftNumber = display.textContent;
-      operator = event.target.id;
-      if (operator !== "equals") {
-        display.textContent += ` ${event.target.textContent} `;
-      }
+      handleOperator(event.target);
       break;
 
     case "clear":
-      reset(display);
+      resetDisplay(display);
 
     default:
       break;
   }
 }
 
+// Global variables - needed for most functions
 let leftNumber;
-let operator;
 let rightNumber;
+let operator;
 
+const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
 
 for (button of buttons) {
